0c290dd1057d7305c8fbcc0bb1f038834c58d27a
18320867985 <18320867985>
2018-08-30 20:05:28 +0800

    20180830

--- js/common/cysDialog.js
+++ js/common/cysDialog.js
@@ -1,11 +1,12 @@
 //预加载询问框
-document.write('<div class="cysShow_window"><h3>是否删除该彩票！</h3><div class="cysShow_window_btn"><a href="javascript:;"  class="no">否</a><a href="javascript:;" style="margin-right:35px;" class="yes">是</a></div></div>');
-document.write("<style>.cysmask{ position:fixed; top:0px; left:0px; width:100%; height:100%; background:rgba(0,0,0,0.5); display:block; z-index:10000; display:none;}<\/style>");
-document.write('<style>.cysShow_window{ width:450px; height:auto; padding-bottom:20px; position:fixed; top:50%; left:50%; margin-left:-225px; padding-top:20px; margin-top:-175px; background:#fff; z-index:10002; display:none; }'
+document.write('<div class="cysShow_window"><h3>是否删除该彩票！</h3><div class="cysShow_window_btn"><a href="javascript:;"  class="no">否</a><a href="javascript:;" style="" class="yes">是</a></div></div>');
+document.write("<style>.cysmask{ position:fixed; top:0px; left:0px; width:100%; height:100%; background:rgba(0,0,0,0.5); display:block; z-index:10000; display:none; }<\/style>");
+document.write('<style>.cysShow_window{ width:400px; height:auto; padding-bottom:20px; position:fixed; top:50%; left:50%; margin-left:-200px; padding-top:20px; margin-top:-175px; background:#fff; z-index:10002; display:none;  border:1px solid #ddd; box-shadow:  0 5px 10px rgba(0,0,0,.5);}'
               + '.cysShow_window h3{ width:100%; font-size:16px; text-align:center; margin-bottom:20px; color:#333;}'
-              + '.cysShow_window_btn{ float:right;}'
-              + '.cysShow_window_btn a{ font-size:14px; margin-right:9px; width:85px; height:35px; line-height:35px; display:block;  float:left; text-align:center; color:#2196f3; }'
+              + '.cysShow_window_btn{ text-align:center;}'
+              + '.cysShow_window_btn a{     font-size: 14px;width: 85px;height: 35px;line-height: 35px;display: inline-block;text-align: center;color: #2196f3;border: 1px solid #ddd;}'
               + '.cysShow_window_btn a:hover{  color:#2196f3; background:#e3f2fd;}'
+              +'.cysShow_window_btn a+a{ margin-left:20px; }'
               + '.cysShow_window_btn a:focus{ background:#bbdefb; color:#2196f3;}<\/style>');
 //调用的函数
 function cysMessageDialog(){
